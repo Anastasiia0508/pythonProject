136694acad88aa46a64ffa740d2c352a6e2ef10e
Anastasiia0508 <slunchenkoav@gmail.com>
2024-07-21 15:17:52 +0300

    цикл while

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,10 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
+if my_list[i] >= 1:
+    print(my_list[i])
 while i < len(my_list):
     i += 1
-    if my_list[i] > 1:
+    if my_list[i] >= 1:
         print(my_list[i])
     if my_list[i] < 0:
         break
